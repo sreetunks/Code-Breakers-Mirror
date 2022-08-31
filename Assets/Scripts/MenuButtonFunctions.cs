abdcbced90860fa07ea0cb0255fb48fc947aeda1
Nathan Minnick <NLMinnick@student.fullsail.edu>
2022-08-31 18:48:38 -0400

    Created a scene for the Main Menu - Nathan Created sub menus for selecting saves, exit conformation, and credit display - Nathan Added the button function script to begin adding functionality to the menus - Nathan Added the game manager script to start tracking the sub menus & to allow moving through the menus successfully - Nathan

---
+++ Assets/Scripts/MenuButtonFunctions.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonFunctions : MonoBehaviour
+{
+    // Button to load the last saved game
+    public void Continue()
+    {
+
+    }
+
+    // Button to start a new game
+    public void NewGame()
+    {
+
+    }
+
+    // Button to load a save file
+    public void LoadGame()
+    {
+
+    }
+
+    // Button to open the settings menu
+    public void Settings()
+    {
+
+    }
+
+    // Button to open the credits screen
+    public void Credits()
+    {
+
+    }
+
+    // Button to close the application
+    public void Exit()
+    {
+        
+    }
+
+    // Button to select a save file
+    public void Select()
+    {
+        
+    }
+
+    // Button to back out of a menu
+    public void Back()
+    {
+
+    }
+
+    // Button to confirm exiting the application
+    public void Yes()
+    {
+    
+    }
+}
