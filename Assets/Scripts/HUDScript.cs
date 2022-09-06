91f876156145f7467c12cc9e9316a05632444594
Atticus Sookma <atticus28@hotmail.com>
2022-09-06 05:49:38 -0400

    HUD action log is working mostly, all scripts can add to it by calling HUDScript.HUD.ActionLogEvent(string eventText) the action log holds 10 events and currently cannot scroll due to some unresolved bugs

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -19,17 +19,26 @@ public class HUDScript : MonoBehaviour
     public Image Ability5Image;
     public Image HackImageLeft;
     public Image HackImageRight;
+    public GameObject ActionLog;
+    public TMP_Text ALText;
     public TMP_Text HealthNumber;
     public TMP_Text APNumber;
+
+    public bool DropDown;
+    private Vector3 ActionLogOrigin;
     //Turn Order List
     //Turn Order Icons
 
+    private List<string> ActionLogText;
     // Start is called before the first frame update
     void Awake()
     {
         HUD = this;
         HackImageLeft.enabled = false;
         HackImageRight.enabled = false;
+        DropDown = false;
+        ActionLogOrigin = ActionLog.transform.localPosition;
+        ActionLogText = new List<string>();
     }
 
     public void UpdateHack()
@@ -40,5 +49,34 @@ public class HUDScript : MonoBehaviour
             HackImageLeft.enabled = true;
             HackImageRight.enabled = true;
         }
+    }
+
+    public void ActionLogEvent(string LogMessage)
+    {
+        ALText.text = "";
+        ActionLogText.Add(LogMessage);
+        foreach(string msg in ActionLogText)
+        {
+            ALText.text += msg + "\n";
+        }
+        if(ActionLogText.Count > 10)
+        {
+            ActionLogText.RemoveAt(0);
+        }
+        //TODO Add small text box that displays the added message outside of the drop down -Atticus
+    }
+    public void ToggleLog()
+    {        
+        var TempTransform = ActionLog.transform.localPosition;
+        if (!DropDown)
+        {
+            TempTransform.y += -510;
+            ActionLog.transform.localPosition =  TempTransform * 4 * Time.deltaTime;
+        }
+        else
+        {
+            ActionLog.transform.localPosition = ActionLogOrigin;
+        }
+        DropDown = !DropDown;
     }
 }
