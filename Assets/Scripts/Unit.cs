9b30fc474e852f357d8f9f5397df0821ff173ea6
Sreenath PS <contact@sreetunks.in>
2022-09-07 07:49:04 +0530

    FIXUP #2: Unit - Fix Pathfinding Checks for Door Tiles

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -104,7 +104,7 @@ public class Unit : MonoBehaviour, IGridObject
             var gridPositionX = new GridPosition(position.X + deltaX, position.Z);
             GridSystem.TryGetGridCellState(gridPositionX, out var cellState);
             var newTargetPositionX = new Vector3(10000, 10000, 10000);
-            if (cellState != GridCellState.Impassable || cellState != GridCellState.Occupied)
+            if (cellState != GridCellState.Impassable && cellState != GridCellState.Occupied)
             {
                 newTargetPositionX = targetPosition - GridSystem.GetWorldPosition(gridPositionX);
             }
@@ -113,7 +113,7 @@ public class Unit : MonoBehaviour, IGridObject
             var gridPositionZ = new GridPosition(position.X, position.Z + deltaZ);
             GridSystem.TryGetGridCellState(gridPositionZ, out cellState);
             var newTargetPositionZ = new Vector3(10000, 10000, 10000);
-            if (cellState != GridCellState.Impassable || cellState != GridCellState.Occupied)
+            if (cellState != GridCellState.Impassable && cellState != GridCellState.Occupied)
             {
                 newTargetPositionZ = targetPosition - GridSystem.GetWorldPosition(gridPositionZ);
             }
