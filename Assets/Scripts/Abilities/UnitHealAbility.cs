e6dbbde593764483134c409b9baa8c8ce9795b4a
Sreenath PS <contact@sreetunks.in>
2022-09-08 08:30:15 +0530

    Abilities - Implement Heal Ability

---
+++ Assets/Scripts/Abilities/UnitHealAbility.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Abilities/Heal")]
+public class UnitHealAbility : AbilityBase
+{
+    [SerializeField] int healAmount;
+    [SerializeField] int cooldownDuration;
+    [SerializeField] int actionPointCost;
+
+    public override int CooldownDuration => cooldownDuration;
+
+    public override int ActionPointCost => actionPointCost;
+
+    public override void Use(Unit owningUnit)
+    {
+        owningUnit.Heal(healAmount);
+    }
+}
