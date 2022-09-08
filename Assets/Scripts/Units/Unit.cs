d161753d114fe9ccdeeba9039cad736948724186
Sreenath PS <contact@sreetunks.in>
2022-09-08 07:15:03 +0530

    Unit - Movement Cleanup

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
 using System.Collections.Generic;
-using Grid;
 using UnityEngine;
+using Grid;
 
 public class Unit : MonoBehaviour, IGridObject
 {
-    
     [SerializeField] private float moveSpeed = 4f;
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private Animator unitAnimator;
-    
-    private Vector3 _targetPosition;
-    private GridPosition _targetGridPosition;
+
     private List<Vector3> _path;
-    
+
     private static readonly int IsWalking = Animator.StringToHash("IsWalking"); // Caching ID for Parameter
 
     public GridCellState GridCellPreviousState { get; set; }
@@ -23,29 +19,29 @@ public class Unit : MonoBehaviour, IGridObject
     private void Awake()
     {
         _path = new List<Vector3>();
-        _targetPosition = transform.position; // Makes sure the unit does not walk to Vector3(0, 0, 0) upon load.
     }
 
     private void Start()
     {
-        var position = transform.position;
-        Position = GridSystem.GetGridPosition(position);
+        Position = GridSystem.GetGridPosition(transform.position);
         GridCellPreviousState = GridCellState.Impassable;
         GridSystem.UpdateGridObjectPosition(this, Position);
-        position = GridSystem.GetWorldPosition(Position);
-        transform.position = position;
-        _targetPosition = position;
+        transform.position = GridSystem.GetWorldPosition(Position);
         IsOnDoorGridCell = CheckIsOnDoorGridCell(GridCellPreviousState);
     }
 
     private void Update()
     {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
         if (_path.Count <= 0) return;
-        print(_path.Count);
         var toTarget = _path[0] - transform.position;
         var dist = toTarget.magnitude;
 
-        if (dist > 0)
+        if (dist > 0.01)
         {
             var move = moveSpeed * Time.deltaTime * toTarget.normalized; // Makes move speed the same for all frame-rate's
 
@@ -61,14 +57,14 @@ public class Unit : MonoBehaviour, IGridObject
         }
         else
         {
-            unitAnimator.SetBool(IsWalking, false); // Ends "Walk" Animations
-
             var newGridPosition = GridSystem.GetGridPosition(transform.position);
             if (newGridPosition == Position) return;
             GridSystem.UpdateGridObjectPosition(this, newGridPosition);
             Position = newGridPosition;
             IsOnDoorGridCell = CheckIsOnDoorGridCell(GridCellPreviousState);
             _path.RemoveAt(0);
+
+            if (_path.Count == 0) unitAnimator.SetBool(IsWalking, false); // Ends "Walk" Animations^M
         }
     }
 
@@ -84,12 +80,10 @@ public class Unit : MonoBehaviour, IGridObject
     {
         var position = Position;
         var targetGridPosition = GridSystem.GetGridPosition(targetPosition);
-        
+
         GridSystem.TryGetGridCellState(targetGridPosition, out var targetCellState);
         if (forceMove)
         {
-            _targetGridPosition = targetGridPosition;
-            _targetPosition = GridSystem.GetWorldPosition(_targetGridPosition);
             Position = targetGridPosition;
             IsOnDoorGridCell = CheckIsOnDoorGridCell(targetCellState);
 
@@ -98,18 +92,20 @@ public class Unit : MonoBehaviour, IGridObject
         else if (targetCellState is GridCellState.Impassable or GridCellState.Occupied)
             return;
 
+        int numIterations = 0;
         do
         {
+            if (numIterations++ > 32) return;
             // TODO: Implement Dijkstra Pathfinding
             // Currently just moves in a simplistic way
-            
+
             //Pathfinding Logic
             var deltaX = targetGridPosition.X - position.X;
             var deltaZ = targetGridPosition.Z - position.Z;
 
             if (deltaX != 0) deltaX /= Mathf.Abs(deltaX);
             if (deltaZ != 0) deltaZ /= Mathf.Abs(deltaZ);
-            
+
             // X Logic
             var gridPositionX = new GridPosition(position.X + deltaX, position.Z);
             GridSystem.TryGetGridCellState(gridPositionX, out var cellState);
@@ -129,17 +125,13 @@ public class Unit : MonoBehaviour, IGridObject
             }
 
             if (newTargetPositionX == new Vector3(10000, 10000, 10000) && newTargetPositionZ == new Vector3(10000, 10000, 10000)) break; // NOTE: Simple path not found, this is where we expand the logic to find a path.
-            
+
             // Compare Logic
             position = newTargetPositionX.magnitude < newTargetPositionZ.magnitude ? gridPositionX : gridPositionZ;
             _path.Add(GridSystem.GetWorldPosition(position));
-           
+
             // TODO: Get 4 Way Pathfinding to work
-        } 
+        }
         while (position != targetGridPosition);
-        
-        // Move Logic - TODO: Update to use Pathfinding
-        _targetGridPosition = targetGridPosition;
-        _targetPosition = GridSystem.GetWorldPosition(_targetGridPosition);
     }
 }
